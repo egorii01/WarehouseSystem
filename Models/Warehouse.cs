dbdee72625e888aa790272a3fef866afbeb3759d
Egor Shramko <egorii2001@gmail.com>
2023-12-27 01:28:23 +0700

    Добавил модель склада

---
+++ Models/Warehouse.cs
@@ -0,0 +1,15 @@
+namespace WarehouseSystem.Models
+{
+
+    public class Warehouse
+    {
+
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+
+    }
+
+}
