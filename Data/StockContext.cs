e2b08123e5cc8ae676ef2b1611a18ce1aa96bddf
Egor Shramko <egorii2001@gmail.com>
2023-12-27 01:59:27 +0700

    Создал контекст базы данных для приложения (надеюсь)

--- Data/StockContext.cs
+++ Data/StockContext.cs
@@ -1,6 +1,25 @@
+using WarehouseSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace WarehouseSystem.Data
 {
 
-    
+    public class StockContext : DbContext
+    {
+
+        public StockContext(DbContextOptions<StockContext> options) : base(options)
+        {
+        }
+
+        //перечисляем множества данных из моделей в контексте БД
+        public DbSet<Check> Checks { get; set; }
+        public DbSet<CheckEntry> CheckEntries { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Import> Imports { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> StockRecords { get; set; }
+    }
 
 }
