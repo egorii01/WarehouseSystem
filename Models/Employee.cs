b302f9c81aa6d8b93cfb1f1325dfe810437aa9ff
Egor Shramko <egorii2001@gmail.com>
2023-12-27 01:39:02 +0700

    Добавил модель сотрудника

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+namespace WarehouseSystem.Models
+{
+
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public Position Position { get; set; }
+
+    }
+
+}
